820782f7801c37a334cd01a48ef86f0cb864fe74
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2020-10-03 18:16:18 +0200

    feat: return codes on login

--- api/src/routes/login.ts
+++ api/src/routes/login.ts
@@ -15,12 +15,18 @@ const LoginRoute = async (server : any, opts : any, next: () => void) => {
         async handler(req: any, res: any) {
             let body = req.body;
             // Getting the accout from db or something, here simulating getting the account by mail
+            // User not found should return { statusCode: 401, error: 'Bad Request', message: 'user_not_found' })
+            // curl -X POST "http://localhost:10000/login" -H "Content-Type: application/json"  --data email:admin@swoopin.green curl -X POST "http://localhost:10000/login" -H "Content-Type: application/json"  --data password:xSsFoDBgizoNYxK2NxOlXvUkeLmpJ9UWdth/EC9a2atZzgadjoMK1dsLV/A=
             let account =  req.conf.account;
 
             let encryptedPwd = EncryptionService.encryptPassword({password: body.password, salt: account.id});
-            let logIn = EncryptionService.comparePassword({ password: body.password, salt: account.id, encrypted: encryptedPwd });
-            let token = server.jwt.sign({ userId: account.id, name: account.name, isAdmin: false })
-            res.send({token});
+            let login = EncryptionService.comparePassword({ password: body.password, salt: account.id, encrypted: encryptedPwd });
+            if (login) {
+                let token = server.jwt.sign({ userId: account.id, name: account.name, isAdmin: false })
+                res.send({token});
+            } else {
+                res.send({ statusCode: 401, error: 'Bad Request', message: 'wrong_credentials' });
+            }
         },
     })
     next()
